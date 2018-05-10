18b594dff670ed886c8180295fdd8d0d929e5f02
Jayram Rout <jayram.rout@gmail.com>
2018-05-09 21:14:57 -0400

    Checking

---
+++ DataType/src/datatype/CharDataTypes.java
@@ -0,0 +1,8 @@
+package datatype;
+
+public class CharDataTypes {
+    public static void main(String[] args) {
+        char symbol = '\u01F8';
+        System.out.println(" Unicode Character : "+symbol);
+    }
+}
